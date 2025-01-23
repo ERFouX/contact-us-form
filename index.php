005f6b40637ff985fb8c6a12a92cd11d5fc914fb
ERFouX <ERFouX@gmail.com>
2025-01-23 22:09:18 +0330

    Adjust HTML indentation to 4 spaces for improved readability

--- index.php
+++ index.php
@@ -1,58 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>Contact Us Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Contact Us Form</title>
 </head>
 <body>
-<div class="background">
-  <div class="container">
-    <div class="screen">
-      <div class="screen-header">
-        <div class="screen-header-left">
-          <a href="closed.php"><div class="screen-header-button close"></div></a>
-          <div class="screen-header-button maximize"></div>
-          <div class="screen-header-button minimize"></div>
+    <div class="background">
+        <div class="container">
+            <div class="screen">
+                <div class="screen-header">
+                    <div class="screen-header-left">
+                        <a href="closed.php"><div class="screen-header-button close"></div></a>
+                        <div class="screen-header-button maximize"></div>
+                        <div class="screen-header-button minimize"></div>
+                    </div>
+                    <div class="screen-header-right">
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                    </div>
+                </div>
+                <div class="screen-body">
+                    <div class="screen-body-item left">
+                        <div class="app-title">
+                            <span>CONTACT</span>
+                            <span>US</span>
+                        </div>
+                        <div class="app-contact">CONTACT INFO : <a class="app-contact" style="text-decoration: none;" href="mailto:ERFouX@Gmail.com">ERFouX@Gmail.com</a></div>
+                    </div>
+                    <div class="screen-body-item">
+                        <div class="app-form">
+                            <div class="app-form-group">
+                                <input name="name" class="app-form-control" placeholder="NAME">
+                            </div>
+                            <div class="app-form-group">
+                                <input name="email" class="app-form-control" placeholder="EMAIL">
+                            </div>
+                            <div class="app-form-group">
+                                <input name="contact_no" class="app-form-control" placeholder="CONTACT NO">
+                            </div>
+                            <div class="app-form-group message">
+                                <input name="message" class="app-form-control" placeholder="MESSAGE">
+                            </div>
+                            <div class="app-form-group buttons">
+                                <button class="app-form-button">CANCEL</button>
+                                <button type="submit" class="app-form-button">SEND</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="screen-header-right">
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
-        </div>
-      </div>
-      <div class="screen-body">
-        <div class="screen-body-item left">
-          <div class="app-title">
-            <span>CONTACT</span>
-            <span>US</span>
-          </div>
-          <div class="app-contact">CONTACT INFO : <a class="app-contact" style="text-decoration: none;" href="mailto:ERFouX@Gmail.com">ERFouX@Gmail.com</a></div>
-        </div>
-        <div class="screen-body-item">
-          <div class="app-form">
-            <div class="app-form-group">
-              <input name="name" class="app-form-control" placeholder="NAME">
-            </div>
-            <div class="app-form-group">
-              <input name="email" class="app-form-control" placeholder="EMAIL">
-            </div>
-            <div class="app-form-group">
-              <input name="contact_no" class="app-form-control" placeholder="CONTACT NO">
-            </div>
-            <div class="app-form-group message">
-              <input name="message" class="app-form-control" placeholder="MESSAGE">
-            </div>
-            <div class="app-form-group buttons">
-              <button class="app-form-button">CANCEL</button>
-              <button type="submit" class="app-form-button">SEND</button>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
 </body>
 </html>
