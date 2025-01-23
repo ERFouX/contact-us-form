20499db8ea3f2b07a3589198057d3bbe0f84db0d
ERFouX <ERFouX@gmail.com>
2025-01-23 21:57:17 +0330

    Add close page for close button

---
+++ closed.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Closed</title>
+    <style>
+        body, html {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .link {
+            color: white;
+            text-decoration: none; 
+            transition: color 0.3s;
+        }
+        .link:hover {
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="background">
+        <div class="container">
+            <div class="app-title">
+                <a href="index.php" class="link">CONTACT US :D</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
